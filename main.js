62868c563f461764ad3f1a19566c706a6d7475a7
Motadeh <adeleyetolu9250@gmail.com>
2018-06-30 12:35:51 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+
+
+
+/* let droplist = document.getElementById('country');
+droplist.length = 0;
+let defaultoption = document.createElement('option');
+defaultoption.text = 'choose country';
+droplist.add(defaultoption);
+droplist.selectedIndex = 0; */
+
+const url = 'https://free.currencyconverterapi.com/api/v5/currencies';
+
+// const url2 = 'https://free.currencyconverterapi.com/api/v5/convert';
+
+// let data;
+// Call currencyOption method not console.log on the data
+fetch(url).then(response => response.json()).then(data => currencyOption(data.results));
+// data;
+
+//  let fromcurrency = document.getElementById('countries');
+
+function currencyOption(data){
+
+    let fromcurrency = document.getElementById('countries');
+    let tocurrency = document.getElementById('countries2');
+
+
+    for (let currency of Object.keys(data).sort()){
+        let {currencyName, id} = data[currency];
+
+        // console.log(currencyName);
+
+
+
+        let optionFrom = document.createElement('option');
+        optionFrom.innerText = `${currency} (${currencyName})`;
+        optionFrom.value = `${currency}`;
+
+        let optionTo = document.createElement('option');
+        optionTo.innerText = `${currency} (${currencyName})`;
+        optionTo.value = `${currency}`;
+
+        fromcurrency.appendChild(optionFrom);
+
+        tocurrency.appendChild(optionTo); // ToCurrencyInput is undefined!
+
+    }
+
+ /* function myfunction(amount, fromcurrency, tocurrency) {
+    document.getElementById("fromcurrency").classList.toggle("show"); */
+
+ }
+
+ if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+  
