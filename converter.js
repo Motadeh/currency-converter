56fefd1322c2c51e6363fc529a5ed5a798d609b0
Tolulope Adeleye <adeleyetolu9250@gmail.com>
2024-06-30 15:17:37 +0100

    Fixes with new API

--- converter.js
+++ converter.js
@@ -1,18 +1,75 @@
+// function myfunction() {
+//     let from = document.getElementById("countries").value;
+//     let to = document.getElementById("countries2").value; 
+//     let fromAmount = document.getElementById("amount").value;
+    
+//     fetch(`https://free.currencyconverterapi.com/api/v5/convert?q=${from}_${to}&compact=ultra`)
+//     .then(response => response.json())
+//     .then(rates => {
+//         for (let rate in rates) {
+//             console.log(rates[rate]);
+//             let calc = rates[rate]; //You need to pass rate back into the object to get the value
+//             let total = (calc * fromAmount); //calculation
+//             console.log(total); 
+//             // toAmount.value = total;
+//             document.getElementById("amount2").innerHTML = `${to} ${total}`;
+
+//         dbPromise.then(db => {
+//             const ty = db.transaction('rates', 'readwrite');
+//             const ratestore = ty.objectStore('rates');
+
+//             ratestore.put({
+//                 rate: calc,
+//                 id: `${from}_${to}`
+//             });
+//             return ty.complete;
+//         });
+//         return rate_value;
+//         }
+//     })
+
+//     const url = `https://free.currencyconverterapi.com/api/v5/currencies/${from}`;
+//     let data = {
+//         Country: 'ayn'
+//     }
+
+//     let fetchdata = {
+//         method: 'POST',
+//         body: data,
+//         headers: new Headers()
+//     }
+
+//     fetch(url, fetchdata).then(function(){
+//         console.log(res);
+//     });
+
+// }
+
+
+/* USING THE NEW API ENDPOINT URL */
+
 function myfunction() {
     let from = document.getElementById("countries").value;
     let to = document.getElementById("countries2").value; 
     let fromAmount = document.getElementById("amount").value;
+
+    // var apiKey = '0YIyK94BA3GuoZyEq61HGBssDYDdyJg6fGHcsHly'
     
-    fetch(`https://free.currencyconverterapi.com/api/v5/convert?q=${from}_${to}&compact=ultra`)
+    fetch(`https://api.freecurrencyapi.com/v1/latest?apikey=${apiKey}&currencies=${to}&base_currency=${from}`)
     .then(response => response.json())
     .then(rates => {
         for (let rate in rates) {
-            console.log(rates[rate]);
-            let calc = rates[rate]; //You need to pass rate back into the object to get the value
-            let total = (calc * fromAmount); //calculation
-            console.log(total); 
+            console.log(rates[rate][to]);
+            // let calc = rates[rate]; //You need to pass rate back into the object to get the value
+            // let total = (calc * fromAmount); //calculation
+            // console.log(total); 
             // toAmount.value = total;
-            document.getElementById("amount2").innerHTML = `${to} ${total}`;
+            // document.getElementById("amount2").innerHTML = `${to} ${total}`;
+
+            let exRate = rates[rate][to];
+
+            document.getElementById("amount2").innerHTML = `${to} ${exRate}`;
+
 
         dbPromise.then(db => {
             const ty = db.transaction('rates', 'readwrite');
@@ -28,19 +85,14 @@ function myfunction() {
         }
     })
 
-    const url = `https://free.currencyconverterapi.com/api/v5/currencies/${from}`;
-    let data = {
-        Country: 'ayn'
-    }
 
-    let fetchdata = {
-        method: 'POST',
-        body: data,
-        headers: new Headers()
-    }
+    // let fetchdata = {
+    //     method: 'GET',
+    //     // body: data,
+    //     headers: new Headers()
+    // }
 
-    fetch(url, fetchdata).then(function(){
-        console.log(res);
-    });
+    // fetch(url).then(response => response.json()).then(data => currencyOption(data.results));
 
 }
+
--- main.js
+++ main.js
@@ -1,23 +1,34 @@
-const url = 'https://free.currencyconverterapi.com/api/v5/currencies';
-fetch(url).then(response => response.json()).then(data => currencyOption(data.results));
+// const url = 'https://free.currencyconverterapi.com/api/v5/currencies';
 
-function currencyOption(data){
+var apiKey = '0YIyK94BA3GuoZyEq61HGBssDYDdyJg6fGHcsHly'
+
+const url = `https://api.freecurrencyapi.com/v1/currencies?apikey=${apiKey}`;
+
+
+fetch(url).then(response => response.json()).then(data => {
+  currencyOption(data.data);
+  console.log(data.data)
+});
+
+// console.log()
+
+function currencyOption(data) {
 
   let fromcurrency = document.getElementById('countries');
   let tocurrency = document.getElementById('countries2');
 
 
-  for (let currency of Object.keys(data).sort()){
-    let {currencyName, id} = data[currency];
+  for (let currency of Object.keys(data).sort()) {
+    let { name, code } = data[currency];
 
-        
+
     let optionFrom = document.createElement('option');
-    optionFrom.innerText = `${currency} (${currencyName})`;
-    optionFrom.value = `${currency}`;
+    optionFrom.innerText = `${currency} (${name})`;
+    optionFrom.value = `${code}`;
 
     let optionTo = document.createElement('option');
-    optionTo.innerText = `${currency} (${currencyName})`;
-    optionTo.value = `${currency}`;
+    optionTo.innerText = `${currency} (${name})`;
+    optionTo.value = `${code}`;
 
     fromcurrency.appendChild(optionFrom);
 
@@ -26,15 +37,14 @@ function currencyOption(data){
   }
 }
 
- if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      });
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/sw.js').then(function (registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function (err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
     });
-  }
-  
+  });
+}
